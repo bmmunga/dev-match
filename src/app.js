958d2125186d573c8393845bec9426a54b21b2e4
Boniface Munga <122830272+bmmunga@users.noreply.github.com>
2025-03-18 16:04:10 +0000

    refactor: connect to DB before server starts

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const { connectDB } = require("./config/database");
 
 const app = express();
 
-PORT = 3000;
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed");
+  });
