d10b90244a6093f8cc9008ce602ef46d36206422
Boniface Munga <122830272+bmmunga@users.noreply.github.com>
2025-03-20 22:33:28 +0000

    feat(api): add initial api endpoints

--- src/app.js
+++ src/app.js
@@ -1,8 +1,76 @@
 const express = require("express");
 require("dotenv").config();
 const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (err) {
+    res.status(400).send("User creation failed:", err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("An error occured");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    userFeed = await User.find({});
+    if (!userFeed) {
+      res.send(404).send("No users found");
+    }
+    res.send(userFeed);
+  } catch (err) {
+    res.status(400).send("An error occured");
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const userById = await User.findById(id);
+    if (!userById) {
+      res.status(404).send("User not found");
+    }
+    res.send(userById);
+  } catch (err) {
+    res.status(400).send("An error occured");
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const userToDelete = await User.findByIdAndDelete(id);
+    if (!userToDelete) {
+      res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("An error occured");
+  }
+});
 
 connectDB()
   .then(() => {
