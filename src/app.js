bde699904f3d1a07eb64e89435ad19826d2ec9d8
Boniface Munga <122830272+bmmunga@users.noreply.github.com>
2025-04-14 13:27:04 +0000

    refactor(app): improve route organization and middleware setup
    - Mount routers on distinct paths for better scalability and clarity
    - Use '/auth' for authentication routes and '/profile' for profile routes

--- src/app.js
+++ src/app.js
@@ -1,75 +1,18 @@
 const express = require("express");
 require("dotenv").config();
 const { connectDB } = require("./config/database");
-const { User } = require("./models/user");
-const { validateSignupData } = require("./utils/validations");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { authenticate } = require("./middlewares/authenticate");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/sign-up", async (req, res) => {
-  try {
-    validateSignupData(req);
+const { authRouter } = require("./routes/auth");
+const { profileRouter } = require("./routes/profile");
 
-    const { firstName, lastName, emailId, password } = req.body;
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const existingUser = await User.findOne({ emailId });
-    if (existingUser) {
-      return res.status(400).json("Invalid signup information");
-    }
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
-    await user.save();
-    res.status(201).send("User created successfully");
-  } catch (err) {
-    res.status(400).send(`User creation failed: ${err.message}`);
-  }
-});
-
-app.post("/sign-in", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId: emailId });
-    if (!user) {
-      return res.status(401).send("Invalid email or password");
-    }
-
-    const isPasswordValid = await user.validatePassword(password);
-    if (isPasswordValid) {
-      const token = await user.getJWT();
-      res.cookie("auth_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 24 * 60 * 60 * 1000,
-      });
-      return res.status(200).send("Login successful");
-    } else {
-      return res.status(401).send("Invalid email or password");
-    }
-  } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
-  }
-});
-
-app.get("/profile", authenticate, (req, res) => {
-  try {
-    const user = req.user;
-    res.status(200).send(user);
-  } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
-  }
-});
+app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
 
 connectDB()
   .then(() => {
